bd2e0e0e77c02947e9eed4a76a9912fda290a6db
Sébastien Jacobin <sebastien.jacobin@epitech.eu>
2017-01-30 23:27:45 +0100

    add icomponent and parser headers files

---
+++ icomponent.hh
@@ -0,0 +1,34 @@
+#ifndef __ICOMPONENT_HPP__
+#define __ICOMPONENT_HPP__
+
+namespace nts
+{
+  enum Tristate
+  {
+    UNDEFINED = (-true),
+    TRUE = true,
+    FALSE = false
+  };
+
+  class IComponent
+  {
+   public:
+    /// Compute value of the precised pin
+    virtual nts::Tristate Compute(size_t pin_num_this = 1) = 0;
+
+    /// Useful to link IComponent together
+    virtual void SetLink(size_t pin_num_this,
+                         nts::IComponent &component,
+                         size_t pin_num_target) = 0;
+
+    ///// Print on terminal the name of the component and
+    //// the state of every pin of the current component
+    /// The output won’t be tested, but it may help you
+    /// as a trace.
+    virtual void Dump(void) const = 0;
+
+    virtual ~IComponent(void) { }
+  };
+}
+
+#endif
