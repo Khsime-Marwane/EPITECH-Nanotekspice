bd1d29d91a29efcb5e1fa0928318282e40516acc
Sébastien Jacobin <sebastien.jacobin@epitech.eu>
2017-02-02 15:09:13 +0100

    createTree() Parser implemented

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -14,28 +14,27 @@
 #include <string>
 #include "IParser.hpp"
 
-#define STRINGS this->treeRoot->children->at(0)
-#define NEWLINES this->treeRoot->children->at(1)
-#define COMPONENTS this->treeRoot->children->at(2)
-#define LINKS this->treeRoot->children->at(3)
-#define LINKSEND this->treeRoot->children->at(4)
-#define SECTIONS this->treeRoot->children->at(5)
-
 class   Parser : public nts::IParser {
-public:
+ public:
 
-Parser(char *file_content);
-virtual ~Parser();
+  Parser(char *file_content);
+  virtual ~Parser();
 
-nts::t_ast_node *getRoot() const;
-void feed(std::string const& input);
-void parseTree(nts::t_ast_node& root);
-nts::t_ast_node *createTree();
-
+  nts::t_ast_node *getRoot() const;
+  void feed(std::string const& input);
+  void parseTree(nts::t_ast_node& root);
+  nts::t_ast_node *createTree();
 
  public:
-nts::t_ast_node *treeRoot;
-std::vector<std::string> file;
+  nts::t_ast_node *treeRoot;
+  std::vector<std::string> file;
+  std::map<std::string, int>  comp_values;
+  nts::t_ast_node *strings_t;
+  nts::t_ast_node *newlines_t;
+  nts::t_ast_node *comps_t;
+  nts::t_ast_node *links_t;
+  nts::t_ast_node *linksend_t;
+  nts::t_ast_node *sections_t;
 };
 
 #endif /* _PARSER_HPP_ */
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -12,26 +12,31 @@ Parser::Parser(char *file_content) {
 
   std::ifstream file_c(file_content);
   std::string   line;
-  if (file.is_open())
-    while (getline(file, line))
+  if (file_c.is_open())
+    while (getline(file_c, line))
       this->file.push_back(line);
   file_c.close();
 
   // Create the tree's root
-    this->treeRoot = new nts::t_ast_node(new std::vector<nts::t_ast_node* >);
+    this->treeRoot = new nts::t_ast_node(new std::vector<nts::t_ast_node*>);
     this->treeRoot->lexeme = "Default";
     this->treeRoot->type = nts::ASTNodeType::DEFAULT;
     this->treeRoot->value = "Default";
 
     for (int i = 0; i < 6; i++) {
-        this->treeRoot->children->push_back(new nts::t_ast_node(NULL));
+        this->treeRoot->children->push_back(new nts::t_ast_node(new std::vector<nts::t_ast_node*>));
         this->treeRoot->children->at(i)->type = (nts::ASTNodeType)i;
     }
+
+  this->strings_t = this->treeRoot->children->at(0);
+  this->newlines_t = this->treeRoot->children->at(1);
+  this->comps_t = this->treeRoot->children->at(2);
+  this->links_t = this->treeRoot->children->at(3);
+  this->linksend_t = this->treeRoot->children->at(4);
+  this->sections_t = this->treeRoot->children->at(5);
 }
 
-Parser::~Parser() {
-
-}
+Parser::~Parser() {}
 
 void    Parser::feed(std::string const& input) {
     if (!input.empty())
@@ -43,6 +48,65 @@ void    Parser::parseTree(nts::t_ast_node &root) {
 }
 
 nts::t_ast_node *Parser::createTree() {
+  if (this->file.size() > 0){
+      int pos;
+      std::string section;
+      for (std::vector<std::string>::iterator it = this->file.begin(); it != this->file.end(); ++it)
+        {
+          if (section == ".chipsets:")
+            {
+              if ((pos = (*it).find('\t')) > 1 || (pos = (*it).find(' ')) > 1)
+                {
+                  nts::t_ast_node  *tmp = new nts::t_ast_node(NULL);
+                  tmp->type = nts::ASTNodeType::COMPONENT;
+                  tmp->lexeme = (*it).substr(0, pos);
+                  pos = (*it).rfind('\t');
+                  tmp->value = (*it).substr(pos + 1);
+                  this->comps_t->children->push_back(tmp);
+                }
+            } else if (section == ".links:")
+              {
+                if ((pos = (*it).find('\t')) > 1 || (pos = (*it).find(' ')) > 1)
+                  {
+                    nts::t_ast_node  *tmp = new nts::t_ast_node(NULL);
+                    nts::t_ast_node  *tmp2 = new nts::t_ast_node(NULL);
+                    tmp->type = nts::ASTNodeType::LINK;
+                    tmp->type = nts::ASTNodeType::LINK_END;
+                    std::string tmp_str((*it).substr(0, pos));
+                    tmp->lexeme = tmp_str.substr(0, tmp_str.find(':'));
+                    tmp->value = tmp_str.substr(tmp_str.find(':') + 1);
+                    this->links_t->children->push_back(tmp);
+                    pos = (*it).rfind('\t');
+                    tmp_str = (*it).substr(pos + 1, (*it).length());
+                    tmp2->lexeme = tmp_str.substr(0, tmp_str.find(':'));
+                    tmp2->value = tmp_str.substr(tmp_str.find(':') + 1);
+                    this->linksend_t->children->push_back(tmp2);
+                  }
+              }
+          if ((*it).length() == 0)
+            {
+              nts::t_ast_node  *tmp = new nts::t_ast_node(NULL);
+              tmp->type = nts::ASTNodeType::NEWLINE;
+              this->newlines_t->children->push_back(tmp);
+            }
+          if ((*it).find('.') == 0)
+            {
+              nts::t_ast_node  *tmp = new nts::t_ast_node(NULL);
+              tmp->type = nts::ASTNodeType::SECTION;
+              section = (*it);
+              tmp->value = (*it);
+              this->sections_t->children->push_back(tmp);
+            }
+          if ((*it).find('#') >= 0 && (*it).find('#') <= (*it).length())
+            {
+              nts::t_ast_node *tmp = new nts::t_ast_node(NULL);
+              tmp->type = nts::ASTNodeType::STRING;
+              tmp->value = (*it).substr((*it).find('#'));
+              this->strings_t->children->push_back(tmp);
+            }
+        }
+      return (this->treeRoot);
+    }
     return NULL;
 }
 
