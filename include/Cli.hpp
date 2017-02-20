78676bdec61cd6b639c96f7afc439f48fd6fce1a
khsime_m <marwane.khsime@epitech.eu>
2017-02-20 15:36:35 +0100

    ok

---
+++ include/Cli.hpp
@@ -0,0 +1,56 @@
+//
+// Author: Marwane Khsime 
+// Date: 2017-02-20 02:22:57 
+//
+// Last Modified by:   Marwane Khsime 
+// Last Modified time: 2017-02-20 02:22:57
+//
+
+#ifndef _CLI_HPP_
+# define _CLI_HPP_
+
+#include "Parser.hpp"
+
+
+/*
+**  The CLI (command line interface) allow to use 
+**  few functions to manipulate the circuit :
+**
+**  • exit - closes the program.
+**
+**  • display - It prints on stdout the value of all outputs sorted by name.
+**
+**  • <input Name>=<value> - It changes the value of an input. (Not a clock)
+**
+**  • simulate - It launches a simulation.
+**
+**  • loop - Simulate without prompting again until SIGINT.
+**
+**  • dump - Call the Dump method of every IComponent
+**
+*/
+class   Cli {
+public:
+    // Constructor / Destructor
+    Cli();
+    ~Cli();
+
+    // Commands avalaible
+    void    Exit();
+    void    Display();
+    void    SetInput(const std::string &name);
+    void    Simulate();
+    void    Loop();
+    void    Dump();
+
+    // Getters
+
+private:
+    Parser parser;
+    
+    // Circuits
+    std::map<std::string, nts::IComponent *> circuits;
+    std::vector<nts::IComponent *> outputs;
+};
+
+#endif /* _CLI_HPP_ */
---
+++ src/Cli.cpp
@@ -0,0 +1,45 @@
+//
+// Author: Marwane Khsime 
+// Date: 2017-02-20 02:22:21 
+//
+// Last Modified by:   Marwane Khsime 
+// Last Modified time: 2017-02-20 02:22:21
+//
+
+#include "Cli.hpp"
+
+Cli::Cli() {
+
+}
+
+Cli::~Cli() {
+
+}
+
+void    Cli::Exit() {
+
+}
+
+void    Cli::Display() {
+
+}
+
+void    Cli::SetInput(const std::string &input) {
+    (void)input;
+}
+
+void    Cli::Simulate() {
+
+}
+
+void    Cli::Loop() {
+
+}
+
+void    Cli::Dump() {
+
+}
+
+int main() {
+    return 0;
+}
